ca4e1115a2f163448a702832da9268a0f1cf8fbf
vasavim27 <vmvasavi@gmail.com>
2025-02-06 13:31:50 -0800

    commiting new changes in files

--- src/encapsulationexamples/Product.java
+++ src/encapsulationexamples/Product.java
@@ -5,4 +5,48 @@ public class Product {
     private double price;
     private int stockQuantity;
 
-}
+    public Product(String name,double price,int stockQuantity){
+        this.name=name;
+        this.price=price;
+        this.stockQuantity=stockQuantity;
+
+    }
+    public String getName() {
+
+        return name;
+
+    }
+    public double getPrice(){
+        return price;
+    }
+    public int getStockQuantity(){
+        return stockQuantity;
+    }
+    public void updatePrice(double price){
+        if (price>0){
+            this.price=price;
+        }
+        else {
+            System.out.println("Please enter a valid price for product "+name);
+        }
+    }
+    public void checkStock(int quantity) {
+        if (quantity > 0 && quantity < stockQuantity) {
+            stockQuantity -= quantity;
+            System.out.println("The remaining stock of Product "+name+" is "+stockQuantity);
+        } else {
+            System.out.println("Insufficient "+name+" Stock!! Please Enter  valid quantity details");
+        }
+    }
+    public static void main(String[] args)
+    {
+        Product product=new Product("Printer",200.00,12);
+        System.out.println("The name of the product purchased is "+product.getName());
+        System.out.println("The stock available for the product "+product.getName()+" is "+product.getStockQuantity());
+        product.updatePrice(210.00);
+        System.out.println("The updated price of the product is "+product.getPrice());
+        product.checkStock(5);
+        product.checkStock(13);
+    }
+
+    }
